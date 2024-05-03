10aa85c4d4706d5d1f3571f394f2726069c6a369
loarte <c.loarte@alumnos.upm.es>
2024-05-03 21:32:01 +0200

    Cambios Torre

--- src/Torre.cpp
+++ src/Torre.cpp
@@ -1,38 +1,151 @@
 #include"Torre.h"
 #include"Pieza.h"
+#include "Tablero.h"
 
 using namespace std;
 
-//Posicion Torres Blancas
+extern Tablero tablero;
+
+//BLANCAS
 extern int TorreB1x;extern int TorreB1y;
 extern int TorreB2x;extern int TorreB2y;
 
-//Posicion Torres Negras
+//NEGRAS
 extern int TorreN1x; extern int TorreN1y;
 extern int TorreN2x; extern int TorreN2y;
 
-void Torre::mover_izq(int casillas) { //Izquierda de la pantalla, no del punto de vista de  la torre. x es el numero de casillas
-	Pieza::mover_izq(casillas);
-}
-void Torre::mover_dch(int casillas) {
-	Pieza::mover_dch(casillas);
-}
-void Torre::mover_arriba(int casillas) { //Izquierda de la pantalla, no del punto de vista de la torre. x es el numero de casillas
-		Pieza::mover_arriba(casillas);	
-}
-void Torre::mover_abajo(int casillas) { //Las torrres se pueden mover para arriba y abajo, y para los lados cuando quieran, hay que meter las restricciones
-		Pieza::mover_abajo(casillas);	
+void Torre::mover_izq(int casillas, int color, int num) { 
+	if (getColor() == 1 && casillas >= 1 && casillas <= 3 && tablero.esPosOcupadaPorBlanca(getPosx(), getPosy() + casillas) == false && tablero.esPosOcupadaPorNegra(getPosx(), getPosy() + casillas) == false) {
+		Pieza::mover_izq(casillas);
+		switch (num)
+		{
+		case 1: TorreB1x = TorreB1x - casillas;
+			break;
+		case 2: TorreB2x = TorreB2x - casillas;
+			break;
+		default:
+			break;
+		}
+	}
+	else if (getColor() == 0 && casillas >= 1 && casillas <= 3 && tablero.esPosOcupadaPorBlanca(getPosx(), getPosy() + casillas) == false && tablero.esPosOcupadaPorNegra(getPosx(), getPosy() + casillas) == false) {
+		Pieza::mover_izq(casillas);
+		switch (num)
+		{
+		case 1: TorreN1y = TorreN1y - casillas;
+			break;
+		case 2: TorreN2y = TorreN2y - casillas;
+			break;
+		default:
+			break;
+		}
+	}
+	else {
+		cout << "La torre " << color << " se sale de los limites del tablero" << endl;
+
+	}
 }
 
-void Torre::mover_arr_dcha(int casillas) {
+void Torre::mover_dch(int casillas, int color, int num) {
+	if (getColor() == 1 && casillas >= 1 && casillas <= 3 && tablero.esPosOcupadaPorBlanca(getPosx(), getPosy() + casillas) == false && tablero.esPosOcupadaPorNegra(getPosx(), getPosy() + casillas) == false) {
+		Pieza::mover_dch(casillas);
+		switch (num)
+		{
+		case 1: TorreB1x = TorreB1x + casillas;
+			break;
+		case 2: TorreB2x = TorreB2x + casillas;
+			break;
+		default:
+			break;
+		}
+	}
+	else if (getColor() == 0 && casillas >= 1 && casillas <= 3 && tablero.esPosOcupadaPorBlanca(getPosx(), getPosy() + casillas) == false && tablero.esPosOcupadaPorNegra(getPosx(), getPosy() + casillas) == false) {
+		Pieza::mover_dch(casillas);
+		switch (num)
+		{
+		case 1: TorreN1y = TorreN1y + casillas;
+			break;
+		case 2: TorreN2y = TorreN2y + casillas;
+			break;
+		default:
+			break;
+		}
+	}
+	else {
+		cout << "La torre " << color << " se sale de los limites del tablero" << endl;
+
+	}
+}
+void Torre::mover_arriba(int casillas, int color, int num) { 
+	if (getColor() == 1 && casillas >= 1 && casillas <= 4 && tablero.esPosOcupadaPorBlanca(getPosx(), getPosy() + casillas) == false && tablero.esPosOcupadaPorNegra(getPosx(), getPosy() + casillas) == false) { 
+		Pieza::mover_arriba(casillas);
+		switch (num)
+		{
+		case 1: TorreB1y = TorreB1y + casillas;
+			break;
+		case 2: TorreB2y = TorreB2y + casillas;
+			break;
+		default:
+			break;
+		}
+	}
+	else if (getColor() == 0 && casillas >= 1 && casillas <= 4 && tablero.esPosOcupadaPorBlanca(getPosx(), getPosy() + casillas) == false && tablero.esPosOcupadaPorNegra(getPosx(), getPosy() + casillas) == false) {
+		Pieza::mover_arriba(casillas);
+		switch (num)
+		{
+		case 1: TorreN1y = TorreN1y + casillas;
+			break;
+		case 2: TorreN2y = TorreN2y + casillas;
+			break;
+		default:
+			break;
+		}
+	}
+	else {
+		cout << "La torre " << color << " se sale de los limites del tablero" << endl;
+
+	}
+}
+
+void Torre::mover_abajo(int casillas, int color, int num) {
+	if (getColor() == 1 && casillas >= 1 && casillas <= 4 && tablero.esPosOcupadaPorBlanca(getPosx(), getPosy() + casillas) == false && tablero.esPosOcupadaPorNegra(getPosx(), getPosy() + casillas) == false) {
+		Pieza::mover_abajo(casillas);
+		switch (num)
+		{
+		case 1: TorreB1y = TorreB1y + casillas;
+			break;
+		case 2: TorreB2y = TorreB2y + casillas;
+			break;
+		default:
+			break;
+		}
+	}
+	else if (getColor() == 0 && casillas >= 1 && casillas <= 4 && tablero.esPosOcupadaPorBlanca(getPosx(), getPosy() + casillas) == false && tablero.esPosOcupadaPorNegra(getPosx(), getPosy() + casillas) == false) {
+		Pieza::mover_arriba(casillas);
+		switch (num)
+		{
+		case 1: TorreN1y = TorreN1y + casillas;
+			break;
+		case 2: TorreN2y = TorreN2y + casillas;
+			break;
+		default:
+			break;
+		}
+	}
+	else {
+		cout << "La torre " << color << " se sale de los limites del tablero" << endl;
+
+	}
+}
+
+void Torre::mover_arr_dcha(int casillas, int color, int num) {
 	cout << "Una torre no puede moverse en diagonal" << endl;
 }
-void Torre::mover_arr_izq(int casillas) {
+void Torre::mover_arr_izq(int casillas, int color, int num) {
 	cout << "Una torre no puede moverse en diagonal" << endl;
 }
-void Torre::mover_abajo_dcha(int casillas) {
+void Torre::mover_abajo_dcha(int casillas, int color, int num) {
 	cout << "Una torre no puede moverse en diagonal" << endl;
 }
-void Torre::mover_abajo_izq(int casillas) {
+void Torre::mover_abajo_izq(int casillas, int color, int num) {
 	cout << "Una torre no puede moverse en diagonal" << endl;
 }
--- src/Torre.h
+++ src/Torre.h
@@ -2,33 +2,35 @@
 
 #include"ETSIDI.h"
 #include "Pieza.h"
+// #include "Tablero.h"
 #include <iostream>
 
 using namespace std;
 
 class Torre :public Pieza {
+
 private:
 	int numTorre;
+
 public:
-	Torre(int numTorre_c = 0, int color_c = 0, int x_c = 0, int y_c = 0, string tipoPieza_c = "Vacio") //Constructor 
+	Torre(int numTorre_c = 0, int color_c = 0, int x_c = 0, int y_c = 0, string tipoPieza_c = "Vacio")
 	{
 		numTorre = numTorre_c;
 		setColor(color_c);
 		setPosx(x_c);
 		setPosy(y_c);
-		setTipo(tipoPieza_c);
-		cout << "Se ha creado la torre de color " << getColor()<<" numero "<<numTorre << " y toma la posicion: " << x_c << y_c << endl;
+		cout << "Se ha creado la torre de color " << getColor() <<" numero "<< numTorre << " y toma la posicion: " << x_c << y_c << endl;
 	}
 	~Torre() { cout << "Torre" << numTorre << " destruida" << endl; }
 	int getNumTorre() { return numTorre; }
 
-	void mover_izq(int casillas);
-	void mover_dch(int casillas);
-	void mover_arriba(int casillas);
-	void mover_abajo(int casillas);
-	void mover_arr_dcha(int casillas);
-	void mover_arr_izq(int casillas);
-	void mover_abajo_dcha(int casillas);
-	void mover_abajo_izq(int casillas);
-	//Estas últimas 4 funciones están comentadas porque la torre no puede desplazarse en diagonal
+	void mover_izq(int casillas, int color, int num);
+	void mover_dch(int casillas, int color, int num);
+	void mover_arriba(int casillas, int color, int num);
+	void mover_abajo(int casillas, int color, int num);
+	void mover_arr_dcha(int casillas, int color, int num);
+	void mover_arr_izq(int casillas, int color, int num);
+	void mover_abajo_dcha(int casillas, int color, int num);
+	void mover_abajo_izq(int casillas, int color, int num);
+	
 };
