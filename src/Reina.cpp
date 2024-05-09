9fc826865759f9955b6137dc0c5011048c689233
jotapacio <61844854+jotapacio@users.noreply.github.com>
2024-05-09 20:28:06 +0200

    Update Reina.cpp

--- src/Reina.cpp
+++ src/Reina.cpp
@@ -8,12 +8,12 @@ extern Tablero tablero;
 
 
 void Reina::mover_izq(int casillas, int color) { //Izquierda de la pantalla, no del punto de vista del peon
-	if (getColor() == 1 && casillas >= 1 && casillas < 4 && tablero.esPosOcupadaPorBlanca(getPosx() - casillas, getPosy()) == false) { //Esto funciona bien para controlar los limites del tablero 
+	if (getColor() == 1 && tablero.esPosOcupadaPorBlanca(getPosx() - casillas, getPosy()) == false) { //Esto funciona bien para controlar los limites del tablero 
 		Pieza::mover_izq(casillas);
 
 	//	ReinaBx = ReinaBx - casillas;
 	}
-	else if (getColor() == 0 && casillas >= 1 && casillas < 4 && tablero.esPosOcupadaPorNegra(getPosx() - casillas, getPosy()) == false) { //Esto funciona bien para controlar los limites del tablero 
+	else if (getColor() == 0 && tablero.esPosOcupadaPorNegra(getPosx() - casillas, getPosy()) == false) { //Esto funciona bien para controlar los limites del tablero 
 		Pieza::mover_izq(casillas);
 
 	//	ReinaNx = ReinaNx - casillas;
@@ -23,12 +23,12 @@ void Reina::mover_izq(int casillas, int color) { //Izquierda de la pantalla, no 
 	}
 }
 void Reina::mover_dch(int casillas, int color) {
-	if (getColor() == 1 && casillas >= 1 && casillas < 4 && tablero.esPosOcupadaPorBlanca(getPosx() + casillas, getPosy()) == false) { //Esto funciona bien para controlar los limites del tablero 
+	if (getColor() == 1 && tablero.esPosOcupadaPorBlanca(getPosx() + casillas, getPosy()) == false) { //Esto funciona bien para controlar los limites del tablero 
 		Pieza::mover_dch(casillas);
 
 	//	ReinaBx = ReinaBx + casillas;
 	}
-	else if (getColor() == 0 && casillas >= 1 && casillas < 4 && tablero.esPosOcupadaPorNegra(getPosx() + casillas, getPosy()) == false) { //Esto funciona bien para controlar los limites del tablero 
+	else if (getColor() == 0 && tablero.esPosOcupadaPorNegra(getPosx() + casillas, getPosy()) == false) { //Esto funciona bien para controlar los limites del tablero 
 		Pieza::mover_dch(casillas);
 
 	//	ReinaNx = ReinaNx + casillas;
@@ -38,12 +38,12 @@ void Reina::mover_dch(int casillas, int color) {
 	}
 }
 void Reina::mover_arriba(int casillas, int color) {
-	if (getColor() == 1 && casillas >= 1 && casillas < 5 && tablero.esPosOcupadaPorBlanca(getPosx(), getPosy() + casillas) == false) { //Esto funciona bien para controlar los limites del tablero 
+	if (getColor() == 1 && tablero.esPosOcupadaPorBlanca(getPosx(), getPosy() + casillas) == false) { //Esto funciona bien para controlar los limites del tablero 
 		Pieza::mover_arriba(casillas);
 
 		//ReinaBy = ReinaBy + casillas;
 	}
-	else if (getColor() == 0 && casillas >= 1 && casillas < 5 && tablero.esPosOcupadaPorNegra(getPosx(), getPosy() + casillas) == false) { //Esto funciona bien para controlar los limites del tablero 
+	else if (getColor() == 0 && tablero.esPosOcupadaPorNegra(getPosx(), getPosy() + casillas) == false) { //Esto funciona bien para controlar los limites del tablero 
 		Pieza::mover_arriba(casillas);
 
 		//ReinaNy = ReinaNy + casillas;
@@ -53,12 +53,12 @@ void Reina::mover_arriba(int casillas, int color) {
 	}
 }
 void Reina::mover_abajo(int casillas, int color) {
-	if (getColor() == 1 && casillas >= 1 && casillas < 5 && tablero.esPosOcupadaPorBlanca(getPosx(), getPosy() - casillas) == false) { //Esto funciona bien para controlar los limites del tablero 
+	if (getColor() == 1 && tablero.esPosOcupadaPorBlanca(getPosx(), getPosy() - casillas) == false) { //Esto funciona bien para controlar los limites del tablero 
 		Pieza::mover_abajo(casillas);
 
 	//	ReinaBy = ReinaBy - casillas;
 	}
-	else if (getColor() == 0 && casillas >= 1 && casillas < 5 && tablero.esPosOcupadaPorNegra(getPosx(), getPosy() - casillas) == false) { //Esto funciona bien para controlar los limites del tablero 
+	else if (getColor() == 0 && tablero.esPosOcupadaPorNegra(getPosx(), getPosy() - casillas) == false) { //Esto funciona bien para controlar los limites del tablero 
 		Pieza::mover_abajo(casillas);
 
 		//ReinaNy = ReinaNy - casillas;
@@ -69,14 +69,14 @@ void Reina::mover_abajo(int casillas, int color) {
 }
 
 void Reina::mover_arr_dcha(int casillas, int color) {
-	if (getColor() == 1 && casillas >= 1 && casillas < 3 && tablero.esPosOcupadaPorBlanca(getPosx() + casillas, getPosy() + casillas) == false) {
+	if (getColor() == 1 && tablero.esPosOcupadaPorBlanca(getPosx() + casillas, getPosy() + casillas) == false) {
 		Pieza::mover_arr_dcha(casillas);
 
 		//ReinaBx = ReinaBx + casillas;
 		//ReinaBy = ReinaBy + casillas;
 
 	}
-	else if (getColor() == 0 && casillas >= 1 && casillas < 3 && tablero.esPosOcupadaPorNegra(getPosx() + casillas, getPosy() + casillas) == false) {
+	else if (getColor() == 0  && tablero.esPosOcupadaPorNegra(getPosx() + casillas, getPosy() + casillas) == false) {
 		Pieza::mover_arr_dcha(casillas);
 
 		//ReinaNx = ReinaNx + casillas;
@@ -88,14 +88,14 @@ void Reina::mover_arr_dcha(int casillas, int color) {
 	}
 }
 void Reina::mover_arr_izq(int casillas, int color) {
-	if (getColor() == 1 && casillas >= 1 && casillas < 3 && tablero.esPosOcupadaPorBlanca(getPosx() - casillas, getPosy() + casillas) == false) {
+	if (getColor() == 1 && tablero.esPosOcupadaPorBlanca(getPosx() - casillas, getPosy() + casillas) == false) {
 		Pieza::mover_arr_izq(casillas);
 
 		//ReinaBx = ReinaBx - casillas;
 		//ReinaBy = ReinaBy + casillas;
 
 	}
-	else if (getColor() == 0 && casillas >= 1 && casillas < 3 && tablero.esPosOcupadaPorNegra(getPosx() - casillas, getPosy() + casillas) == false) {
+	else if (getColor() == 0 && tablero.esPosOcupadaPorNegra(getPosx() - casillas, getPosy() + casillas) == false) {
 		Pieza::mover_arr_izq(casillas);
 
 	//	ReinaNx = ReinaNx - casillas;
@@ -107,14 +107,14 @@ void Reina::mover_arr_izq(int casillas, int color) {
 	}
 }
 void Reina::mover_abajo_dcha(int casillas, int color) {
-	if (getColor() == 1 && casillas >= 1 && casillas < 3 && tablero.esPosOcupadaPorBlanca(getPosx() + casillas, getPosy() - casillas) == false) {
+	if (getColor() == 1 && tablero.esPosOcupadaPorBlanca(getPosx() + casillas, getPosy() - casillas) == false) {
 		Pieza::mover_abajo_dcha(casillas);
 
 	//	ReinaBx = ReinaBx + casillas;
 	//	ReinaBy = ReinaBy - casillas;
 
 	}
-	else if (getColor() == 0 && casillas >= 1 && casillas < 3 && tablero.esPosOcupadaPorNegra(getPosx() + casillas, getPosy() - casillas) == false) {
+	else if (getColor() == 0 && tablero.esPosOcupadaPorNegra(getPosx() + casillas, getPosy() - casillas) == false) {
 		Pieza::mover_abajo_dcha(casillas);
 
 	//	ReinaNx = ReinaNx + casillas;
@@ -126,14 +126,14 @@ void Reina::mover_abajo_dcha(int casillas, int color) {
 	}
 }
 void Reina::mover_abajo_izq(int casillas, int color) {
-	if (getColor() == 1 && casillas >= 1 && casillas < 3 && tablero.esPosOcupadaPorBlanca(getPosx() - casillas, getPosy() - casillas) == false) {
+	if (getColor() == 1 && tablero.esPosOcupadaPorBlanca(getPosx() - casillas, getPosy() - casillas) == false) {
 		Pieza::mover_abajo_izq(casillas);
 
 	//	ReinaBx = ReinaBx - casillas;
 	//	ReinaBy = ReinaBy - casillas;
 
 	}
-	else if (getColor() == 0 && casillas >= 1 && casillas < 3 && tablero.esPosOcupadaPorNegra(getPosx() - casillas, getPosy() - casillas) == false) {
+	else if (getColor() == 0 && tablero.esPosOcupadaPorNegra(getPosx() - casillas, getPosy() - casillas) == false) {
 		Pieza::mover_abajo_izq(casillas);
 
 	//	ReinaNx = ReinaNx - casillas;
