ee93b4220aefa8bf0e53fce17a3fd85a3cf266b0
dani-sato <61557139+dani-sato@users.noreply.github.com>
2024-06-07 23:49:08 +0200

    Update boardgl.h
    boardgl.h final

--- src/boardgl.h
+++ src/boardgl.h
@@ -18,68 +18,71 @@ using namespace std;
 class TableroGL {
 
 public:
-	bool selectedPiece = false;
-	int selected_x, selected_y;
-	TableroGL(Tablero* pb) :m_tablero(pb) {
-		width = 0.15;				//width of each cell in the grid
-		filas = pb->getFilas();		//Grid filasxcolumnas
-		columnas = pb->getColumnas();
-		dist = 2;					//distance of viewpoint from center of the board
-		center_z = 0;
-	}
-	virtual ~TableroGL() {}
+    bool selectedPiece = false;
+    int selected_x, selected_y;
+    TableroGL(Tablero* pb) :m_tablero(pb) {
+        width = 0.15;				//width of each cell in the grid
+        filas = pb->getFilas();		//Grid filasxcolumnas
+        columnas = pb->getColumnas();
+        dist = 2;					//distance of viewpoint from center of the board
+        center_z = 0;
+    }
+    virtual ~TableroGL() {}
 
-	//Initizalization
-	void init();						//enable lights
+    //Initizalization
+    void init();						//enable lights
 
-	
-	//Event managers
-	void dibuja();
-	void DrawGridSilver();
-	void DrawCell(int i, int j);
-	void KeyDown(unsigned char key);
-	void SpecialKeyDown(unsigned char key);
-	void MouseButton(int x, int y, int button, bool down, bool shiftKey, bool ctrlKey);
-	void moveRandomBlackPiece();
-	void timerMoveBlackPiece(int value);
 
-	//info
-	void setSize(int filas, int columnas) { this->filas = filas; this->columnas = columnas; }
-	int getFilas() { return filas; }
-	int getColumnas() { return columnas; }
+    //Event managers
+    void dibuja();
+    void DrawGrid();
+    //	void DrawGridSilver();
 
-	//coord
-	void cell2center(int cell_x, int cell_y, float& glx, float& gly) {
-		//cell_x, cell_y are the board cell coordinates (upper left hand corner is (0,0))
-		//glx, gly refer to the center of the cell(cell_x,cell_y) in world coordinates
-		glx = cell_y * width + width / 2.0f;
-		gly = -cell_x * width - width / 2.0f;
-	}
+    void DrawCell(int i, int j);
+    void KeyDown(unsigned char key);
+    void SpecialKeyDown(unsigned char key);
+    void MouseButton(int x, int y, int button, bool down, bool shiftKey, bool ctrlKey);
+    void moveRandomBlackPiece();
+    void timerMoveBlackPiece(int value);
 
-	void world2cell(double x, double y, int& cell_x, int& cell_y) {
-		//world coordinates to cell
-		cell_x = (int)(abs(y / width));
-		cell_y = (int)(x / width);
-	}
+    //info
+    void setSize(int filas, int columnas) { this->filas = filas; this->columnas = columnas; }
+    int getFilas() { return filas; }
+    int getColumnas() { return columnas; }
+
+    //coord
+    void cell2center(int cell_x, int cell_y, float& glx, float& gly) {
+        //cell_x, cell_y are the board cell coordinates (upper left hand corner is (0,0))
+        //glx, gly refer to the center of the cell(cell_x,cell_y) in world coordinates
+        glx = cell_y * width + width / 2.0f;
+        gly = -cell_x * width - width / 2.0f;
+    }
+
+    void world2cell(double x, double y, int& cell_x, int& cell_y) {
+        //world coordinates to cell
+        cell_x = (int)(abs(y / width));
+        cell_y = (int)(x / width);
+    }
 
 protected:
-	float width;
-	int filas, columnas;								 //size 
-	Tablero* m_tablero;
+    float width;
+    int filas, columnas;								 //size 
+    Tablero* m_tablero;
+    double center_x, center_y, center_z;
+    double dist;
+    vector<pair<int, int>> posiblesMovimientos;
 
-	//visualization	
-	double center_x, center_y, center_z;
-	double dist;
 
-	//mouse	
-	int xcell_sel, ycell_sel;			//cell selected with mouse
+    //mouse	
+    int xcell_sel, ycell_sel;			//cell selected with mouse
 
-	//mouse+special-keys combination state events 
-	bool controlKey;
-	bool shiftKey;
-	bool leftButton;
-	bool rightButton;
-	bool midButton;
+    //mouse+special-keys combination state events 
+    bool controlKey;
+    bool shiftKey;
+    bool leftButton;
+    bool rightButton;
+    bool midButton;
 };
 
 #endif
+
